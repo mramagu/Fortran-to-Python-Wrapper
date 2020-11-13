ba5427324a90a3007cb5b5efaf1b11fb6453af57
mramagu <73698658+mramagu@users.noreply.github.com>
2020-11-13 12:54:09 +0100

    Add files via upload

---
+++ FortranExamples/TestModule1.f90
@@ -0,0 +1,25 @@
+!  FortranExamples.f90 
+!
+!  FUNCTIONS:
+!  FortranExamples - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: FortranExamples
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program FortranExamples
+
+    implicit none
+
+    ! Variables
+
+    ! Body of FortranExamples
+    print *, 'Hello World'
+
+    end program FortranExamples
+
