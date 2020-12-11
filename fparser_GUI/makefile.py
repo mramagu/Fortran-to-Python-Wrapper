e6f84c42864ddecd567034e6ec77237314a6be68
pablo.romero.ramos@alumnos.upm.es <pablo.romero.ramos@alumnos.upm.es>
2020-12-11 17:39:24 +0100

    Makefile update.

--- fparser_GUI/makefile.py
+++ fparser_GUI/makefile.py
@@ -2,37 +2,54 @@ import subprocess
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-def selectOS(ui):
-    OperatingSystem=ui.comboBox_makeOS.currentText()
-    print(OperatingSystem)
+class Makefile():
+    def __init__(self,self_fparser):
+        self.ui=self_fparser.ui
+        self.self_fparser=self_fparser
+        self.os=''
+        self.condaEnv=''
+        self.env=''
+        self.FC=''
 
-def select_condaEnv(ui):
-    if ui.radioButton_makeYes.isChecked() == True:
-        condaEnv=ui.radioButton_makeYes.text()
-        print(condaEnv)
-    elif ui.radioButton_makeNo.isChecked() == True:
-        condaEnv=ui.radioButton_makeNo.text()
-        print(condaEnv)
+    def selectOS(self):
+        self.os=self.ui.comboBox_makeOS.currentText()
+        print(self.os)
 
-def writeEnv(ui):
-    Env=ui.lineEdit_makeEnv.text()
-    print(Env)
+    def select_condaEnv(self):
+        if ui.radioButton_makeYes.isChecked() == True:
+            self.condaEnv=self.ui.radioButton_makeYes.text()
+            print(self.condaEnv)
+        elif ui.radioButton_makeNo.isChecked() == True:
+            self.condaEnv=self.ui.radioButton_makeNo.text()
+            print(self.condaEnv)
 
-def fcompiler(ui):
-    FC=ui.comboBox_makeFC.currentText()
-    print(FC)
+    def writeEnv(self):
+        self.env=self.ui.lineEdit_makeEnv.text()
+        print(self.env)
 
-def properties(ui):
-    selectOS(ui)
-    select_condaEnv(ui)
-    writeEnv(ui)
-    fcompiler(ui)
+    def fcompiler(self):
+        if self.ui.comboBox_makeFC.currentIndex() == 0:
+            self.FC='intelvem'
+            print(self.FC)
+        elif self.ui.comboBox_makeFC.currentIndex() == 1:
+            self.FC='gfortran'
+            print(self.FC)
+
+    def searchFC(self):
+        p=subprocess.run(['f2py','-c','--help-fcompiler'],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        self.self_fparser.terminal_text.add_line(p.stdout,number=2)
+
+    def properties(self):
+        self.selectOS()
+        self.select_condaEnv()
+        self.writeEnv()
+        self.fcompiler()
     
-def runmake(ui):
-    pass
-    #subprocess.run(['f2py','Hello_world.f90','-m','','-h','Interface.pyf','--overwrite-signature'])
-    #flags=['--fcompiler=' %fcompiler% ,'--f90flags=-O3','--f90flags=-Wno-conversion','--f90flags=-std=f95','--f90flags=/real-size:64' -L%library%]
-    #subprocess.run(['f2py','-c','Interface.pyf','Hello_world.f90',flags])
+    def runmake(self):
+        pass
+        #subprocess.run(['f2py','Hello_world.f90','-m','','-h','Interface.pyf','--overwrite-signature'])
+        #flags=['--fcompiler='+self.FC ,'--f90flags=-O3','--f90flags=-Wno-conversion','--f90flags=-std=f95','--f90flags=/real-size:64' -L%library%]
+        #subprocess.run(['f2py','-c','Interface.pyf','Hello_world.f90',flags])
     
 """
     def f(ui,mf):
@@ -48,4 +65,5 @@ if __name__ == "__main__":
 
     p=subprocess.run(['python','--version'],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     print(p.stdout)
+
 
